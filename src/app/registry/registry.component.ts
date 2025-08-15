2c9e9857950d816d92342ef4eb527db229a3ddd1
Dakota Thompson <58233459+dakthomps00@users.noreply.github.com>
2025-08-14 20:58:40 -0400

    update

--- src/app/registry/registry.component.ts
+++ src/app/registry/registry.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-registry',
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './registry.component.html',
   styleUrl: './registry.component.scss'
 })
-export class RegistryComponent {
+export class RegistryComponent implements AfterViewInit {
+
+  constructor(private renderer: Renderer2) { }
+
+  ngAfterViewInit(): void {
+    // This is where you can add any additional logic after the view has been initialized
+    // For example, you could initialize a third-party library or manipulate the DOM
+    if (!document.querySelector('script[src="https://widget.zola.com/js/widget.js"]')) {
+      const script = this.renderer.createElement('script');
+      script.src = 'https://widget.zola.com/js/widget.js';
+      script.async = true;
+      this.renderer.appendChild(document.body, script);
+    } else {
+      // If it's already loaded, manually trigger the Zola script again
+      if ((window as any).Zola) {
+        (window as any).Zola.render();
+      }
+    }
+  }
+
+
 
 }
